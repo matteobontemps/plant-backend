51a989534499c0c9a517ada5846da684dfdfb13f
SSlhx <sofiane.salih.eca@gmail.com>
2025-05-07 09:48:32 +0200

    ajout AuthController et fonction register

---
+++ src/Controller/Api/AuthController.php
@@ -0,0 +1,61 @@
+<?php 
+namespace App\Controller\Api;
+use App\Entity\User;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Dom\Entity;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AuthController extends AbstractController
+{ 
+    #[Route('/register', name: 'register')]
+    public function register(Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $passwordHasher): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+         if(!isset($data['login'], $data['email'], $data['password'], $data['nom'])){
+            return new JsonResponse(['error' => 'Invalid input'], 400);
+         }
+        if($em->getRepository(User::class)->findOneBy(['email'=>$data['email']]))
+        {
+            return new JsonResponse(['error'=>'email already use'],409);
+        }
+        if($em->getRepository(User::class)->findOneBy(['login'=>$data['login']]))
+        {
+            return new JsonResponse(['error'=>'login already use'],409);
+        }
+
+        try{
+        $user = new User();
+        $user->setIdUser(uniqid());
+        $user->setLogin($data['login']);
+        $user->setEmail($data['email']);
+        $hasherP = $passwordHasher->hashPassword($user, $data['password']);
+        $user->setPassword($hasherP);
+        $user->setNom($data['nom']);
+
+        $em->persist($user);
+        $em->flush();
+
+        return new JsonResponse(['message' => 'User registered successfully'], 201);
+        }catch(\Exception $e){
+            return new JsonResponse(['error'=>'server error', 'details'=>$e->getMessage()],500);
+        }
+    }
+    #[Route('/login', name: 'login')]
+    public function login()
+    {
+        
+
+    }
+
+    #[Route('/dashboard', name: 'dashboard')]
+    public function dashboard()
+    {
+        
+        return $this->render('dashboard.html.twig');
+    }
+}
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,9 +6,11 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User
+class User implements PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\Column(length: 50)]
